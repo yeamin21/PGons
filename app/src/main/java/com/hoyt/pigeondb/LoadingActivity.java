cb4ecc27cbba52b85750cc891b68da9e4d8a3dc8
yeami <yeamin21@outlook.com>
2020-02-08 02:30:32 +0600

    Update

---
+++ app/src/main/java/com/hoyt/pigeondb/LoadingActivity.java
@@ -0,0 +1,40 @@
+package com.hoyt.pigeondb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoadingActivity extends AppCompatActivity {
+    FirebaseAuth fath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+       fath = FirebaseAuth.getInstance();
+       final Intent  i = new Intent(this, TabbetdlayoutActivity.class);
+        final Intent login=new Intent(this,Login.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (fath.getCurrentUser() != null) {
+                    startActivity(i);
+                    finish();
+                }
+                else
+                {
+                    startActivity(login);
+                    finish();
+                }
+
+
+        }
+},3000);
+    }
+}
+
+
+
--- app/src/main/java/com/hoyt/pigeondb/pairs/AddPair.java
+++ app/src/main/java/com/hoyt/pigeondb/pairs/AddPair.java
@@ -2,6 +2,7 @@ package com.hoyt.pigeondb.pairs;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -28,14 +29,13 @@ import java.util.Calendar;
 
 public class AddPair extends AppCompatActivity {
 
-
-
+EditText asD;
     Button addpair;
-    EditText pF, pM, asD;
     Calendar clndr;
     int mYear, mMonth, mDay;
-    AutoCompleteTextView atx;
-ArrayList<Pigeons>p=new ArrayList<>();
+    AutoCompleteTextView pF,pM;
+    DatabaseReference rf;
+    DatabaseReference rc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -43,23 +43,20 @@ ArrayList<Pigeons>p=new ArrayList<>();
         setContentView(R.layout.activity_add_pair);
 
 
-        final DatabaseReference rf = FirebaseDatabase.getInstance().getReference(FirebaseAuth.getInstance().getUid()).child("Pairs");
-        DatabaseReference rc = FirebaseDatabase.getInstance().getReference(FirebaseAuth.getInstance().getUid()).child("Pigeons");
+      rf = FirebaseDatabase.getInstance().getReference(FirebaseAuth.getInstance().getUid()).child("Pairs");
+rc = FirebaseDatabase.getInstance().getReference(FirebaseAuth.getInstance().getUid()).child("Pigeons");
 
-          final ArrayAdapter<String>  adptrHen=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item);
-
-
+         final ArrayAdapter<String> adptrHen = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item);
+        final ArrayAdapter<String> adptrCock= new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item);
 
         clndr = Calendar.getInstance();
         mYear = clndr.get(Calendar.YEAR);
         mDay = clndr.get(Calendar.DAY_OF_MONTH);
         mMonth = clndr.get(Calendar.MONTH);
-        atx=findViewById(R.id.autoCompleteTextView);
         addpair = findViewById(R.id.addPair);
         pF = findViewById(R.id.txt_pgnFather);
         pM = findViewById(R.id.txt_pgnMother);
         asD = findViewById(R.id.assDate);
-
 
 
         asD.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +65,7 @@ ArrayList<Pigeons>p=new ArrayList<>();
                 new DatePickerDialog(AddPair.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        asD.setText(dayOfMonth + "-" + (month + 1 )+ "-" + year);
+                        asD.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
                     }
                 }, mYear, mMonth, mDay).show();
             }
@@ -81,7 +78,7 @@ ArrayList<Pigeons>p=new ArrayList<>();
                 String sM = pM.getText().toString().trim();
                 String dA = asD.getText().toString();
                 Pairs p = new Pairs(sF, sM, dA);
-                rf.child(sF+sM).child("Info").setValue(p).addOnSuccessListener(new OnSuccessListener<Void>() {
+                rf.child(sF + sM).child("Info").setValue(p).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
@@ -92,18 +89,23 @@ ArrayList<Pigeons>p=new ArrayList<>();
         });
 
 
-
         rc.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            for(DataSnapshot ds: dataSnapshot.getChildren())
-            {
-                String gender=ds.child("Basic Info").child("gender").getValue(String.class);
-                String sug=ds.child("Basic Info").child("pigeonID").getValue(String.class);
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String gender = ds.child("Basic Info").child("gender").getValue(String.class).trim();
+                    String sug = ds.child("Basic Info").child("pigeonID").getValue(String.class).trim();
+                    if(gender.equals("Hen"))
+                    {
+                        adptrHen.add(sug);
+                    }
+                    else if (gender.equals("Cock"))
+                    {
+                       adptrCock.add(sug);
+                    }
+//
 
-                    adptrHen.add(sug);
-
-            }
+                }
 
 
             }
@@ -113,12 +115,15 @@ ArrayList<Pigeons>p=new ArrayList<>();
 
             }
         });
-        atx.setAdapter(adptrHen);
+
+        pM.setAdapter(adptrHen);
+        pF.setAdapter(adptrCock);
 
     }
-
-
 }
+
+
+
 
 
 
